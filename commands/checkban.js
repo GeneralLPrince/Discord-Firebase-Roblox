068524c67095c70c875d4a228059a315f05e19cf
GeneralLPrince <53830063+GeneralLPrince@users.noreply.github.com>
2021-04-09 18:34:36 +0100

    Create checkban.js

---
+++ commands/checkban.js
@@ -0,0 +1,57 @@
+module.exports = {
+	name: 'checkban',
+	description: 'Checkban command',
+	
+    execute(suspect, message, firebase) {
+        const nbx = require('noblox.js');
+
+        async function exec() {
+            const UserId = await nbx.getIdFromUsername(suspect).catch(e => "User not found");
+
+            if (UserId == "User not found") {
+                message.channel.send({
+                    embed: {
+                        "type": "rich",
+                        "color": 3092790,					
+                        "title": "",
+                        "description": "We couldn't find the Roblox account, are you sure you typed out your username correctly? Try again please.",
+                    }
+                })
+            }
+            else {
+                var Reference = firebase.database().ref("Bans/")
+                Reference.once("value").then(function(snapshot) {
+                    var IsBanned = snapshot.child(UserId).hasChildren()
+
+                    if (IsBanned == false) {
+                        message.channel.send({
+                            embed: {
+                                "type": "rich",
+                                "color": 3092790,					
+                                "title": "",
+                                "description": suspect+" isn't banned",
+                            }
+                        }) 
+                    }
+                    else if (IsBanned == true) {
+                        Reference.once("value", function(snapshot) {
+                            snapshot.forEach(function(childSnapshot) {
+                                const Moderator = childSnapshot.val().Moderator
+                                const Reason = childSnapshot.val().Reason
+                                message.channel.send({
+                                    embed: {
+                                        "type": "rich",
+                                        "color": 3092790,					
+                                        "title": "",
+                                        "description": "**Username: **"+suspect+"**\nReason:** "+Reason+"\n**Moderator:** "+Moderator,
+                                    }
+                                })   
+                            })
+                        })
+                    }
+                })
+            }
+        }
+        exec()
+    }
+}; 
