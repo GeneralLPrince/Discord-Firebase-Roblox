b5694da18a0f9551f89eaf74c524a7d1e1eb463c
Outer_Salem (Sim) <68174000+outersalemsim@users.noreply.github.com>
2021-05-20 18:11:28 -0400

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -4,6 +4,7 @@ module.exports = {
 	execute(suspect, reason, message, database) {   
 
     const nbx = require('noblox.js');
+    const Discord = require('discord.js');
     async function exec() {
       const userId = await nbx.getIdFromUsername(suspect).catch(e => "User not found");
       if (userId !== "User not found") {
@@ -14,9 +15,22 @@ module.exports = {
           Moderator: message.member.user.tag,
           Reason: reason
           });
-          return message.channel.send("`"+playerName+"` has been banned by: `"+message.member.user.tag+"` for `"+reason+"`")
+
+          const BanSuccess = new Discord.MessageEnbed()
+          .setColor('#07C902')
+          .setDescription('Success!')
+          .addFields(
+            {name: 'Banned Player:', value: playerName},
+            {name: 'Reason:', value: reason}
+          )
+
+          const BanError = new Discord.MessageEnbed()
+          .addFields(
+            {name: 'Error!', value: 'User does not exist! !ban <user> <reason>'}
+            )
+          return message.channel.send(BanSuccess)
       } else {
-        return message.channel.send("User does not exist, please use this format: `!ban [Username][banReason]`")
+        return message.channel.send(BanError)
       }
       }
       exec()
